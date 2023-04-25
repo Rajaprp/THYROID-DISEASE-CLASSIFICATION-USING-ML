c4c592e3513f371286497dc06b56a16b645439dd
Rajaprp <131744826+Rajaprp@users.noreply.github.com>
2023-04-25 01:12:45 -0700

    Add files via upload

---
+++ THYROID DISEASE CLASSIFICATION/Thyroid/flask/templates/app.py
@@ -0,0 +1,6 @@
+import pandas as pandas
+import numpy as np
+import matploylip.pyplot as plt 
+import tensorflow 
+from tenserflow.keras.models import Sequential 
+from tensorflow.keras.models import Layer,Dense,Dropout
